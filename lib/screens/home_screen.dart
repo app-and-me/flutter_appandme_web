80a17e29055058b8bb3dda21e8c26175a9de8807
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-04-19 17:18:37 +0900

    feat. add 스터디 / 네트워킹

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -90,8 +90,46 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ],
                   ),
-                  const Column(children: []),
-                  const Column(children: []),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset('assets/img/study1.png'),
+                      const Text(
+                        '개발 및 디자인 스터디',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const Text(
+                        '선배와 후배가 함께하는 스터디를 꾸준히 진행하여 부원들의 실력\n향상에 도움이 되고자 합니다. 스터디를 통해 영양가 있는 동아리\n시간을 보내면서 부원들 간의 동기부여가 될 수 있도록 합니다.',
+                        style: TextStyle(
+                          color: secondaryColor,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset('assets/img/networking1.png'),
+                      const Text(
+                        '선후배 네트워킹',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const Text(
+                        '선후배 간의 격식없이 다같이 친한 동아리로, 모르는 전공\n부분이나 학교 생활 간의 궁금한 점이 있으면 바로 선배들과\n이야기를 통해 조언이나 팁을 얻으며 친목을 다질 수 있고\n분기마다 동아리 회식으로 인해 더더욱 친밀하고 가까운\n사이입니다.',
+                        style: TextStyle(
+                          color: secondaryColor,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               )),
             ),
