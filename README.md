261c9ca1bc15a26485365cb638de7899576a6d11
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-04-19 10:06:33 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_appandme_web
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_appandme_web/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_appandme_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
