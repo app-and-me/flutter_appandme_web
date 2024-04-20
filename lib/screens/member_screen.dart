bad5cc3729a6ced993269b5e1019601ebbd7b544
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-04-20 14:02:18 +0900

    feat. separate header widget

--- lib/screens/member_screen.dart
+++ lib/screens/member_screen.dart
@@ -1,42 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/header_widget.dart';
+
 class MemeberScreen extends StatelessWidget {
   const MemeberScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Column(
         children: [
-          //header
-          Container(
-            padding:
-                const EdgeInsets.only(left: 70, top: 30, right: 70, bottom: 30),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Row(
-                  children: [
-                    Text('부원소개'),
-                    SizedBox(width: 30),
-                    Text('지원하기'),
-                    SizedBox(width: 30),
-                    Text('QnA'),
-                    SizedBox(width: 30),
-                    Text('활동기록'),
-                    SizedBox(width: 30),
-                    Text('질문하기'),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Image.asset('assets/img/logo.png'),
-                  ],
-                ),
-              ],
-            ),
-          ),
           // header
+          HeaderWidget(),
         ],
       ),
     );
