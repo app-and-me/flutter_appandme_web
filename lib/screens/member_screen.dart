f8a312838e338cacb13411dd9af6bc337d8a1f29
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-04-20 14:00:28 +0900

    feat. header

--- lib/screens/member_screen.dart
+++ lib/screens/member_screen.dart
@@ -5,8 +5,40 @@ class MemeberScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('hi1'),
+    return Scaffold(
+      body: Column(
+        children: [
+          //header
+          Container(
+            padding:
+                const EdgeInsets.only(left: 70, top: 30, right: 70, bottom: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Row(
+                  children: [
+                    Text('부원소개'),
+                    SizedBox(width: 30),
+                    Text('지원하기'),
+                    SizedBox(width: 30),
+                    Text('QnA'),
+                    SizedBox(width: 30),
+                    Text('활동기록'),
+                    SizedBox(width: 30),
+                    Text('질문하기'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset('assets/img/logo.png'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          // header
+        ],
+      ),
     );
   }
 }
